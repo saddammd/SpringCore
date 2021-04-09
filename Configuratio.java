cefefdd427e1f0035f0df85af3b9aee44728c18c
saddammd <62531673+saddammd@users.noreply.github.com>
2021-04-09 19:30:52 +0530

    Add files via upload

---
+++ Configuratio.java
@@ -0,0 +1,42 @@
+package com.spring.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@ComponentScan("com.spring.core.*")
+@PropertySource("classpath:rough.properties")
+public class Configuratio {
+
+	@Bean
+    public static PropertySourcesPlaceholderConfigurer
+                    propertySourcesPlaceHolderConfigurer() {
+        
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+
+	@Bean
+	@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+	public EngineOil engineOil() {
+		
+		 return new EngineOil();
+	}
+	
+	
+	@Bean
+	@Scope("singleton")
+	public Engine engine() {
+		
+		Engine eng = new Engine(engineOil());
+		
+		return eng;
+	}
+	
+
+}
